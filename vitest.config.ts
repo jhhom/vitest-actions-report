32cb2ecbd945eb5811cfc2618bcde4e9eb57b2e9
jhm <maxosen64@gmail.com>
2024-08-27 22:07:50 +0800

    f

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     reporters: ["html", "default"],
-    outputFile: "./test-report/index.html",
+    outputFile: "./test-report-2/index.html",
   },
 });
